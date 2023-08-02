baf0749432a7fc73110d70523f0397b031939ef8
05taoguo <3180229739@qq.com>
2023-08-02 16:57:17 +0800

    实现mybatis-plus增删查改
    实现mybatis-plus增删查改

--- Spring/MyBatisPlus/mybatisplus_01_quickstart/mybatisplus_01_quickstart/src/test/java/cn/jzyh/Mybatisplus01QuickstartApplicationTests.java
+++ Spring/MyBatisPlus/mybatisplus_01_quickstart/mybatisplus_01_quickstart/src/test/java/cn/jzyh/Mybatisplus01QuickstartApplicationTests.java
@@ -2,6 +2,8 @@ package cn.jzyh;
 
 import cn.jzyh.dao.UserDao;
 import cn.jzyh.domain.User;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +15,52 @@ class Mybatisplus01QuickstartApplicationTests {
 
     @Autowired
     private UserDao userDao;
+
+    //    新增
+    @Test
+    void insert_all(){
+        User user = new User();
+        user.setName("jzyh");
+        user.setPassword("331231");
+        user.setTel("轻轻");
+        user.setAge(22);
+        userDao.insert(user);
+    }
+
+    //    删除
+    @Test
+    void delete_all(){
+        userDao.deleteById(1686653353915969538L);
+    }
+
+    //    修改
+    @Test
+    void update_all(){
+        User user = new User();
+        user.setId(1L);
+        user.setName("张三");
+        userDao.updateById(user);
+    }
+
+    //查询单个
+    @Test
+    void selectId_all(){
+        User user = userDao.selectById(1L);
+        System.out.println(user);
+    }
+
+    //    分页查询,配置拦截器
+    @Test
+    void GetByPage(){
+        IPage page = new Page(1,5);
+        userDao.selectPage(page,null);
+        System.out.println("当前页码值" + page.getCurrent());
+        System.out.println("每页显示条数" + page.getSize());
+        System.out.println("一共多少页" + page.getPages());
+        System.out.println("一共多少条数据" + page.getTotal());
+        System.out.println("数据" + page.getRecords());
+
+    }
 
     @Test
     void testGetAll() {
