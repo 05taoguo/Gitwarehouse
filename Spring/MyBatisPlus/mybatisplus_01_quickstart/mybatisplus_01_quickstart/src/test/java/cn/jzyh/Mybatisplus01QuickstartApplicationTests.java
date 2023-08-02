20c70f92479e59cde6652ddd6a70ba488314f9f1
05taoguo <3180229739@qq.com>
2023-08-02 11:53:57 +0800

    Java
    Java学习

--- Spring/MyBatisPlus/mybatisplus_01_quickstart/mybatisplus_01_quickstart/src/test/java/cn/jzyh/Mybatisplus01QuickstartApplicationTests.java
+++ Spring/MyBatisPlus/mybatisplus_01_quickstart/mybatisplus_01_quickstart/src/test/java/cn/jzyh/Mybatisplus01QuickstartApplicationTests.java
@@ -1,13 +1,23 @@
 package cn.jzyh;
 
+import cn.jzyh.dao.UserDao;
+import cn.jzyh.domain.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class Mybatisplus01QuickstartApplicationTests {
 
+    @Autowired
+    private UserDao userDao;
+
     @Test
-    void contextLoads() {
+    void testGetAll() {
+        List<User> userList = userDao.selectList(null);
+        System.out.println(userList);
     }
 
 }
