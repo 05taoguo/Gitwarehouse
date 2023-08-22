0271ceadfb7e0280744e299e92009c5eddf00907
05taoguo <3180229739@qq.com>
2023-08-22 10:24:14 +0800

    测试

--- Takeaway/takeaway_01/src/main/java/cn/jzyh/Java1.java
+++ Takeaway/takeaway_01/src/main/java/cn/jzyh/Java1.java
@@ -3,6 +3,6 @@ package cn.jzyh;
 public class Java1 {
     public static void main(String[] args) {
         System.out.println("niaho");
-        System.out.println("123");
+        System.out.println("123454");
     }
 }
