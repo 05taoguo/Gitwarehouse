d33e6b4f3039c026b9ab2442b5f4e6bdbfa15c7b
05taoguo <3180229739@qq.com>
2023-08-29 10:37:55 +0800

    无

--- Takeaway/takeaway_01/src/main/java/cn/jzyh/Takeaway01Application.java
+++ Takeaway/takeaway_01/src/main/java/cn/jzyh/Takeaway01Application.java
@@ -16,6 +16,7 @@ public class Takeaway01Application {
     public static void main(String[] args) {
         log.info("启动成功。。。");
         SpringApplication.run(Takeaway01Application.class, args);
+        System.out.println("启动");
     }
 
 }
